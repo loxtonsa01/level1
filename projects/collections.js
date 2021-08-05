6cbdb303190c1bc27b4486a4d28cde7ff539c91d
sara loxton <78565764+loxtonsa01@users.noreply.github.com>
2021-08-05 07:45:38 +0000

    Finished the future Job code. Added 3 elements to the array, and made a randomiser for the job picking.

--- projects/collections.js
+++ projects/collections.js
@@ -7,12 +7,17 @@ const jobs = [
   'firefighter',
   'police officer',
   'judge',
+  'scientist',
+  'software engineer',
 ]
 
 function futureJob() {
   let index = 0
+    //getting the last element in the array
+  //let lastElement = jobs[jobs.length - 1]
+  let randomElement = jobs[Math.floor(Math.random()*jobs.length)]
 
-  alert('Your future job will be: ' + jobs[0])
+  alert('Your future job will be: ' + randomElement)
 }
 
 // ####################################
@@ -22,17 +27,19 @@ function futureJob() {
 let subjects = [
   'English',
   'Digital Technology',
+  'Science',
+  'Mathematics'
 ]
 
 function addSubject() {
-  let newSubject = 'Maths'
+  let newSubject = prompt('Pick a subject.')
 
   // add a new subject to the end of the subjects array
   subjects.push(newSubject)
 }
 
 function removeSubject() {
-  let index = 0
+  let index = subjects[subjects.length - 1]
 
   // remove 1 subject at the index position of the subjects array
   subjects.splice(index, 1)
